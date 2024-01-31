5e0c9288e7899c2b8fe46adbd052ee022b161a0e
Paul King <paulk@asert.com.au>
2024-02-01 09:02:58 +1000

    trivial refactor

--- src/main/groovy/dynamic/Main.groovy
+++ src/main/groovy/dynamic/Main.groovy
@@ -10,12 +10,12 @@ int knapsack(Integer[][] dp, int[] v, int[] w, int W, int n) {
     if (W <= 0 || n >= v.length) {
         return 0
     }
-    if (dp[n][W] != null) {
+    if (dp[n][W]) {
         return dp[n][W]
     }
-    int value1 = w[n] > W ? 0 : v[n] + knapsack(dp, v, w, W - w[n], n + 1)
-    int value2 = knapsack(dp, v, w, W, n + 1)
-    dp[n][W] = max(value1, value2)
+    int tryN = w[n] > W ? 0 : v[n] + knapsack(dp, v, w, W - w[n], n + 1)
+    int skipN = knapsack(dp, v, w, W, n + 1)
+    dp[n][W] = max(tryN, skipN)
 }
 
 int[] values = [1, 5, 10, 15, 17]  // Gem values
