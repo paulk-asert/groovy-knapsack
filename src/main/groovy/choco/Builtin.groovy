a795efe9f71f249cb365a06652b9024f88240795
Paul King <paulk@asert.com.au>
2024-02-02 20:05:29 +1000

    formatting

--- src/main/groovy/choco/Builtin.groovy
+++ src/main/groovy/choco/Builtin.groovy
@@ -9,13 +9,12 @@ int W = 10
 int unbounded = 100000
 
 var counts = new IntVar[values.length]
-var totalWeight, totalValue
 var found = false
 
 new Model('KnapsackProblem').with {
-    counts.indices.each { counts[it] = intVar("count$it", 0, W) }
-    totalValue = intVar("Total value for capacity $W (unbounded)", 0, unbounded)
-    totalWeight = intVar("Total weight taken", 0, W)
+    counts.indices.each(i -> counts[i] = intVar("count$i", 0, W))
+    var totalValue = intVar("Total value for capacity $W (unbounded)", 0, unbounded)
+    var totalWeight = intVar("Total weight taken", 0, W)
     knapsack(counts, totalWeight, totalValue, weights, values).post()
     setObjective(MAXIMIZE, totalValue)
 
--- src/main/groovy/choco/Constraints.groovy
+++ src/main/groovy/choco/Constraints.groovy
@@ -9,13 +9,12 @@ int W = 10
 int unbounded = 100000
 
 var counts = new IntVar[values.length]
-var total
 var found = false
 
 new Model('KnapsackProblem').with {
-    counts.indices.each { counts[it] = intVar("count$it", 0, W) }
+    counts.indices.each(i -> counts[i] = intVar("count$i", 0, W))
     scalar(counts, weights, '<=', W).post()
-    total = intVar("Total value for capacity $W (unbounded)", 0, unbounded)
+    var total = intVar("Total value for capacity $W (unbounded)", 0, unbounded)
     scalar(counts, values, '=', total).post()
     setObjective(MAXIMIZE, total)
 
