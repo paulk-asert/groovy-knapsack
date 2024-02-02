a137df87d5639934a84747a64e13604ef72c3bb0
Paul King <paulk@asert.com.au>
2024-02-02 15:42:09 +1000

    additional example

---
+++ src/main/groovy/choco/Builtin.groovy
@@ -0,0 +1,28 @@
+package choco
+
+import org.chocosolver.solver.Model
+import org.chocosolver.solver.variables.IntVar
+
+int[] values = [1, 5, 10, 15, 17]  // Gem values
+int[] weights = [1, 2, 3, 5, 6]    // Weights
+int W = 10
+int unbounded = 100000
+
+var counts = new IntVar[values.length]
+var totalWeight, totalValue
+
+var found = new Model('KnapsackProblem').with {
+    counts.indices.each {counts[it] = intVar("count$it", 0, W) }
+    totalValue = intVar("Total value for capacity $W (unbounded)", 0, unbounded)
+    totalWeight = intVar("Total weight taken", 0, W)
+    knapsack(counts, totalWeight, totalValue, weights, values).post()
+    setObjective(MAXIMIZE, totalValue)
+    solver.solve()
+}
+if (found) {
+    println counts
+    println totalWeight
+    println totalValue
+} else {
+    println 'No solution'
+}
